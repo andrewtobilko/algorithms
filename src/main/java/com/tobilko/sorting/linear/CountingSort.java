bec3c631e7da6f54954e5f3f118c9e12eca7623b
Andrew Tobilko <andrew.tobilko@gmail.com>
2017-09-24 00:48:33 +0300

    CountingSort

--- src/main/java/com/tobilko/sorting/linear/CountingSort.java
+++ src/main/java/com/tobilko/sorting/linear/CountingSort.java
@@ -3,38 +3,46 @@ package com.tobilko.sorting.linear;
 import java.util.Arrays;
 
 /**
- *
  * INPUT: integers in the range 0 to k, k = O(n) -> Θ(n).
- *
+ * <p>
  * For each x, counts the number of elements less than x.
- *
- *
+ * <p>
+ * A[0, n] - the input
+ * B[0, n] - the output
+ * C[0, k] - the temporary working storage
+ * <p>
  * Created by Andrew Tobilko on 9/20/17.
  */
 public final class CountingSort {
 
-    public static void sort(int[] array) {
+    private final static int K = 10;
 
-        System.out.println("> " + Arrays.toString(array));
+    public static int[] sort(int[] A) {
 
-        int A = array.length;
+        // initialise some stuff
+        int n = A.length;
 
-        int B[] = new int[A];
-        int C[] = new int[100];
+        int B[] = new int[n];
+        int C[] = new int[K];
 
-        for (int i = 0; i < A; i++) {
-            C[array[i]] = C[array[i]] + 1;
-        }
+        // count the number of occurrences an array[i] and save into a C[array[i]]
+        for (int i = 0; i < n; ++C[A[i]], ++i) ;
 
-        System.out.println();
+        // count the number of elements that less than or equal to i
+        for (int i = 1; i < K; C[i] += C[i - 1], ++i) ;
 
+        // place an A[i] into its correct sorted position in the B
+        for (int i = n - 1; i >= 0; B[C[A[i]] - 1] = A[i], --C[A[i]], --i) ;
 
-
-        System.out.println("< " + Arrays.toString(B));
+        return B;
     }
 
     public static void main(String[] args) {
-        sort(new int[] {23, 2, 4, 12, 13, 11, 34, 32, 45, 100, 23, 34, 76, 53, 98});
+        int[] input = new int[]{1, 2, 2, 4, 7, 6, 5, 5, 4, 2, 5, 8, 9, 8, 7, 2};
+        int[] output = sort(input);
+
+        System.out.println("> " + Arrays.toString(input));
+        System.out.println("< " + Arrays.toString(output));
     }
 
 }
