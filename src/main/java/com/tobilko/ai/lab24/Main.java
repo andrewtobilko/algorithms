9fc543b0d8d5875dce4a559bbe16ee9e3c62907a
Andrew Tobilko <andrew.tobilko@gmail.com>
2018-05-18 19:43:32 +0300

    A_STAR_SEARCH

--- src/main/java/com/tobilko/ai/lab24/Main.java
+++ src/main/java/com/tobilko/ai/lab24/Main.java
@@ -1,10 +1,12 @@
-package com.tobilko.ai.lab2;
+package com.tobilko.ai.lab24;
 
 
 import com.google.common.collect.Sets;
 
 import java.util.*;
 import java.util.stream.Collectors;
+
+import static java.util.stream.Collectors.toSet;
 
 /**
  * Created by Andrew Tobilko on 3/24/18.
@@ -18,9 +20,103 @@ public final class Main {
         final State initialState = new State(new HashSet<>(ALL), FINAL_STATE, true, null);
         DFS(initialState);
         BFS(initialState);
+        A_STAR_SEARCH(initialState);
     }
 
-    public static void DFS(State initialState) {
+    private static void A_STAR_SEARCH(State initialState) {
+        Set<State> closedSet = new HashSet<>();
+        Set<State> openSet = new HashSet<>(Arrays.asList(initialState));
+
+        Map<State, State> cameFrom = new HashMap<>();
+        Map<State, Integer> gScore = new HashMap<>();
+        gScore.put(initialState, 1);
+
+        Map<State, Integer> fScore = new HashMap<>();
+        fScore.put(initialState, estimateHeuristicCost(initialState));
+
+        while (openSet.size() > 0) {
+
+            Map.Entry<State, Integer> currentEntry = fScore.entrySet().stream().min(Comparator.comparing(Map.Entry::getValue)).get();
+            State currentState = currentEntry.getKey();
+
+            if (currentState.getRightBank().containsAll(ALL)) {
+                State cS = new State(currentState.getLeftBank(), currentState.getRightBank(), currentState.isDirection(), currentState.getBoat());
+                List<State> states = new ArrayList<State>() {{ add(null);}};
+                states.add(cS);
+
+                while (cameFrom.get(cS) != null) {
+                    cS = cameFrom.get(cS);
+                    states.add(cS);
+                }
+
+                //states.stream().filter(Objects::nonNull).forEach(System.out::println);
+                System.out.print("A* = ");
+                System.out.print(states.size());
+                System.out.println(" " + closedSet.size());
+
+                return;
+            }
+
+            openSet.remove(currentState);
+            closedSet.add(currentState);
+            fScore.remove(currentState);
+
+
+            final Set<Set<String>> combinations = getCombinations(currentState.isDirection() ? currentState.getLeftBank() : currentState.getRightBank());
+
+            final boolean currentDirection = !currentState.isDirection();
+
+
+            final List<State> neighbors = combinations
+                    .stream()
+                    .map(c -> new State(new HashSet<>(currentState.getLeftBank()), new HashSet<>(currentState.getRightBank()), currentDirection, c))
+                    .collect(Collectors.toList());
+
+            for (State neighbor : neighbors) {
+
+                if (closedSet.contains(neighbor)) {
+                    continue;
+                }
+
+                if (!openSet.contains(neighbor)) {
+
+                    openSet.add(neighbor);
+
+                    (!currentDirection ? neighbor.getLeftBank() : neighbor.getRightBank()).removeAll(neighbor.getBoat());
+                    (!currentDirection ? neighbor.getRightBank() : neighbor.getLeftBank()).addAll(neighbor.getBoat());
+
+//                    if (!neighbor.isValid()) {
+//                        continue;
+//                    }
+                }
+
+//                (!currentDirection ? neighbor.getLeftBank() : neighbor.getRightBank()).removeAll(neighbor.getBoat());
+//                (!currentDirection ? neighbor.getRightBank() : neighbor.getLeftBank()).addAll(neighbor.getBoat());
+
+//                if (!neighbor.isValid()) {
+//                    continue;
+//                }
+
+                int tentativeGScore = gScore.get(currentState) + 1;
+                if (tentativeGScore >= gScore.getOrDefault(neighbor, Integer.MAX_VALUE)) {
+                    continue;
+                }
+
+                cameFrom.put(neighbor, currentState);
+                gScore.put(neighbor, tentativeGScore);
+                fScore.put(neighbor, gScore.get(neighbor) + estimateHeuristicCost(neighbor));
+
+            }
+
+        }
+
+    }
+
+    private static int estimateHeuristicCost(State from) {
+        return ALL.size() - from.getRightBank().size();
+    }
+
+    private static void DFS(State initialState) {
         visited.add(initialState);
         goToNextStateDFS(initialState);
 
@@ -30,7 +126,7 @@ public final class Main {
         System.out.println(" DFS = " + solutionSteps.size() + " " + visitedStates);
     }
 
-    public static void BFS(State initialState) {
+    private static void BFS(State initialState) {
         visited = new HashSet<>();
         visited.add(initialState);
         State state = goToNextStateBFS(initialState);
@@ -77,7 +173,7 @@ public final class Main {
 
                 if (isFinalState(stateFromQueue)) {
                     solutionSteps.add(stateFromQueue);
-                    System.out.println("The final state is " + stateFromQueue);
+                    //System.out.println("The final state is " + stateFromQueue);
                     return stateFromQueue;
                 }
             }
@@ -132,11 +228,10 @@ public final class Main {
         }
     }
 
-
     private static Set<State> visited = new HashSet<>();
 
     private static Set<Set<String>> getCombinations(Set<String> set) {
-        return Sets.powerSet(set).stream().filter(s -> s.size() > 0 && s.size() <= 3 && s.size() <= set.size()).filter(State::isValid).collect(Collectors.toSet());
+        return Sets.powerSet(set).stream().filter(s -> s.size() > 0 && s.size() <= 3 && s.size() <= set.size()).filter(State::isValid).collect(toSet());
     }
 
     private static boolean isFinalState(State state) {
@@ -151,6 +246,10 @@ class State {
     private Set<String> boat;
 
     private boolean direction;
+
+    public Set<String> getBoat() {
+        return boat;
+    }
 
     public boolean isDirection() {
         return direction;
@@ -179,8 +278,8 @@ class State {
         if (bank.size() == 0) {
             return true;
         }
-        final Set<String> knights = bank.stream().filter(p -> p.substring(0, 1).equals("K")).collect(Collectors.toSet());
-        final Set<String> pages = bank.stream().filter(p -> p.substring(0, 1).equals("P")).collect(Collectors.toSet());
+        final Set<String> knights = bank.stream().filter(p -> p.substring(0, 1).equals("K")).collect(toSet());
+        final Set<String> pages = bank.stream().filter(p -> p.substring(0, 1).equals("P")).collect(toSet());
 
 
         if (knights.size() == 0) {
